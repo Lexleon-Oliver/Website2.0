b536d2d50c0cba4492fb7134fa7ae30271ce239d
Lexleon Oliver <lexleonmg@gmail.com>
2026-07-08 22:07:18 -0300

    ✨ feat: Atualiza URL da imagem no artigo sobre posicionamento de roteador

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -478,7 +478,7 @@ export class BlogService {
         `,
         "subtitle": "E você? Como está posicionado o roteador na sua casa? Já usa a frequência de 5 GHz ou uma rede Mesh?",
         "date": "08/07/2026 19:35:00",
-        "url": "https://cdn.pixabay.com/photo/2016/11/22/21/42/adult-1850703_1280.jpg",
+        "url": "https://unsplash.com/pt-br/fotografias/vista-de-perto-do-homem-conectando-o-cabo-ao-roteador-sem-fio-conceito-de-internet-sem-fio-zv3KZMdXtBA",
         "type": "Artigo"
       }
 
